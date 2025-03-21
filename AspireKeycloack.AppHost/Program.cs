0a6f23f05c0775ce91ea3f7d76df21e0a04ea18e
Sergey Nosov <sergey.nosov@orderfactory.com>
2025-03-21 19:18:59 -0400

    Add Keycloak integration with realm import and HTTPS support
    Implemented Keycloak integration via a new `KeycloakExtensions` class, adding support for HTTPS endpoints and realm import functionality. Adjusted project dependencies, added supporting resources, and optimized certificate handling for development.

--- AspireKeycloack.AppHost/Program.cs
+++ AspireKeycloack.AppHost/Program.cs
@@ -1,15 +1,24 @@
 using AspireKeycloack.AppHost;
+using Projects;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-var weatherApi = builder.AddProject<Projects.backend>("backend");
+var keycloak = builder.AddHttpsKeycloak("keycloak", 3443, 3444)
+        .WithRealmImport("./Realms")
+    ;
+
+// the backend port number 3445 comes from its launchSettings.json
+var weatherApi = builder.AddProject<backend>("backend")
+        .WaitFor(keycloak)
+        .WithReference(keycloak)
+    ;
 
 builder.AddNpmApp("frontend", "../frontend", "dev")
     .WaitFor(weatherApi)
-    .WithEnvironment("BROWSER", "none")
-    .WithHttpsEndpoint(env: "VITE_PORT")
-    .WithEnvironment("VITE_API_URL", weatherApi.GetEndpoint("https"))
+    .WithHttpsEndpoint(3446, env: "VITE_PORT")
     .RunWithHttpsDevCertificate("CERT_PATH", "CERT_KEY_PATH")
-;
+    .WithEnvironment("BROWSER", "none")
+    .WithEnvironment("VITE_API_URL", weatherApi.GetEndpoint("https"))
+    ;
 
 builder.Build().Run();
