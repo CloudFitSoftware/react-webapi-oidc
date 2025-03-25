865ea438d4ce6cbbbf828038c2a775eb51457ca0
Sergey Nosov <sergey.nosov@orderfactory.com>
2025-03-25 11:15:51 -0400

    Add authentication and authorization with OIDC integration
    Integrated OpenID Connect (OIDC) for user authentication using `react-oidc-context` and set up environment variables for configuration. Implemented role-based authorization in the new `Header` component. Updated application structure and dependencies to support authentication flow.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import {AuthProvider} from "react-oidc-context";
+
+const oidcConfig = {
+    authority: import.meta.env.VITE_AUTH_AUTHORITY,
+    client_id: import.meta.env.VITE_AUTH_CLIENT_ID,
+    redirect_uri: import.meta.env.VITE_AUTH_REDIRECT_URI,
+};
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <AuthProvider {...oidcConfig}>
+            <App/>
+        </AuthProvider>
+    </StrictMode>,
 )
