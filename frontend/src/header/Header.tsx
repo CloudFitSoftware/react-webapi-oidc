41cdd3470e0fc7c8975cd2f3de3b47472a70c41e
Sergey Nosov <sergey.nosov@orderfactory.com>
2025-03-26 01:16:57 -0400

    Extract authorization logic into a reusable hook.
    Moved the authorization logic from the `Header` component to a new custom hook `useAuthorization`. This improves code reusability, readability, and separation of concerns. The `Header` component now utilizes the new hook to manage authorization state.

--- frontend/src/header/Header.tsx
+++ frontend/src/header/Header.tsx
@@ -1,50 +1,12 @@
-import {useEffect, useState} from 'react';
 import {useAuth} from 'react-oidc-context';
-
-async function sha256(message: string) {
-    const msgBuffer = new TextEncoder().encode(message);
-    const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer);
-    const hashArray = Array.from(new Uint8Array(hashBuffer));
-    return hashArray.map(b => b.toString(16).padStart(2, '0')).join('').toUpperCase();
-}
+import useAuthorization from '../hooks/useAuthorization.ts';
 
 export function Header() {
 
     const auth = useAuth();
-
+    const isAuthorized = useAuthorization();
     const login = () => auth.signinRedirect();
-
     const logout = () => auth.signoutRedirect();
-
-    const [isAuthorized, setIsAuthorized] = useState(false);
-
-    useEffect(() => {
-        const requiredRoleHash = import.meta.env.VITE_ROLE_HASH.toUpperCase() ?? '';
-
-        // Extract roles from the user profile
-        const roles: string[] = Array.isArray(auth.user?.profile?.role) ? auth.user.profile.role : [];
-
-        const checkAuthorization = async () => {
-            try {
-                for (const role of roles) {
-                    const roleHash = await sha256(role);
-                    if (roleHash === requiredRoleHash) {
-                        setIsAuthorized(true);
-                        return;
-                    }
-                }
-                setIsAuthorized(false);
-            } catch (error) {
-                console.error('Error checking authorization:', error);
-                setIsAuthorized(false);
-            }
-        };
-
-        if (auth.user?.profile?.role) {
-            checkAuthorization().then();
-        }
-
-    }, [auth.user]);
 
     if (auth.isLoading) {
         return <div>Loading...</div>;
